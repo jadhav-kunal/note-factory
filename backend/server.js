9374136ce4732739c5fa938103a3b0abcd529396
Kunal <jadhavkunal1999@gmail.com>
2021-07-16 12:39:16 +0530

    created basic api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 const express = require("express");
-
+const notes= require("./data/notes");
 const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("API is running...")
+});
+
+app.get('/api/notes',(req,res)=>{
+    res.json(notes)
+})
+
 app.listen(5000,console.log("Server started on PORT 5000"));
 
