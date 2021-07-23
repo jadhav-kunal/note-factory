84a0531e86ae847d63406ad873551f5f8ec787b5
Kunal <jadhavkunal1999@gmail.com>
2021-07-23 11:16:18 +0530

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,24 @@
 const express = require("express");
-const notes= require("./data/notes");
+const notes = require("./data/notes");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
-app.get('/',(req,res)=>{
-    res.send("API is running...")
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.get('/api/notes',(req,res)=>{
-    res.json(notes)
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
-app.get('/api/notes/:id',(req,res)=>{
-    const note=notes.find((n)=>n._id===req.params.id);
-    res.send(note);
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
 });
-
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
-
- 
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
